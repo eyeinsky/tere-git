9341e430826cbfc7e3804d72a5683cc2c1399907
eyeinsky <eyeinsky9@gmail.com>
2019-12-13 22:03:42 +0200

    Deklareerime v6idu :)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # See on programm, mis palub kasutajal midagi 5elda
 # ja siis prindib selle v2lja.
 
+# Kommentaar ylal olemas :)
 
 # nyyd olen pythoni failis main.py
 
@@ -11,7 +12,7 @@ while True:
     tekst = input('Ytle midagi pliis: ')
     print('Sa ytlesid: ' + tekst + '!')
 
-    if tekst == 'l6peta':
+    if tekst == 'l6peta': # ja if klausel siin samuti :)
         print('V2ljun programmist')
         break;
     else:
